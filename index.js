8eab76451d0e7996d55f9742ef4534c01a58a499
Arunkumar <arunkumarnura18@gmail.com>
2026-02-19 17:39:45 +0530

    changes made

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const connectDatabase = require("./config/connectDB");
 
 const corsOptions = {
   origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: "*",
 };
 
+app.use(cors(corsOptions)); // MUST be first
 app.use(express.json());
-app.use(cors(corsOptions));
-// app.options("/*", cors(corsOptions));
 
 connectDatabase();
 
