e72679c04c8b9cc485b1c786e8f027dfc516d8c8
Arunkumar <arunkumarnura18@gmail.com>
2026-02-19 11:35:28 +0530

    edited

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
-const cors = require("cors");
+
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 const connectDatabase = require("./config/connectDB");
-app.options("*", cors());
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }),
-);
 
+const cors = require("cors");
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 connectDatabase();
 
 const port = process.env.PORT || 3000;
@@ -24,7 +28,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Project One Api." }); // json format
 });
 
-app.use(express.json());
 const blog = require("./routes/blog");
 const auth = require("./routes/auth");
 const user = require("./routes/user");
