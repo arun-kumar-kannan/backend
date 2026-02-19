bb45b161c4ba38d18bce2d69112c90cfd3ec72f8
Arunkumar <arunkumarnura18@gmail.com>
2026-02-19 11:36:21 +0530

    modified

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
-
 const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 const connectDatabase = require("./config/connectDB");
-
-const cors = require("cors");
 
 const corsOptions = {
   origin: ["http://localhost:3000"],
@@ -19,21 +17,18 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+
 connectDatabase();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  // res.send("Project One Api.");
-  res.json({ message: "Project One Api." }); // json format
+  res.json({ message: "Project One Api." });
 });
 
-const blog = require("./routes/blog");
-const auth = require("./routes/auth");
-const user = require("./routes/user");
-app.use("/api/v1", blog);
-app.use("/api/v1", auth);
-app.use("/api/v1", user);
+app.use("/api/v1", require("./routes/blog"));
+app.use("/api/v1", require("./routes/auth"));
+app.use("/api/v1", require("./routes/user"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
